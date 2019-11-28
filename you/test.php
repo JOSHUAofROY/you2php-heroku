7ee2207cfe5aee496bef631275c7803b7a8a3e81
Joshua <1294686101@qq.com>
2019-11-28 16:32:00 +0800

    Update test.php

--- you/test.php
+++ you/test.php
@@ -1,6 +1,10 @@
 <?php
 require 'Youtube.php';
-
+$url = parse_url($_SERVER['REQUEST_URI']);
+if(!isset($_GET['roy'])){
+    echo "nonono";
+} else {
 $youtube = new Zarkiel\Media\Youtube();
-$links = $youtube->getDownloadLinks('q_gfD3nvh-8');
+$links = $youtube->getDownloadLinks($_GET['roy']);
 print_r($links);
+}
