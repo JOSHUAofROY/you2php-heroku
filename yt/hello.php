2dfc77aa4e2362a0b83b276f46e030667da74bbb
Joshua <1294686101@qq.com>
2019-11-28 18:07:26 +0800

    Update hello.php

--- yt/hello.php
+++ yt/hello.php
@@ -1,8 +1,14 @@
 <?php
-$ch = curl_init();
-$str ='https://api.youtubemultidownloader.com/video?url=https://www.youtube.com/watch?v=AD6DHSTjiwI';
-curl_setopt($ch, CURLOPT_URL, $str);
-curl_setopt ( $ch, CURLOPT_RETURNTRANSFER, true );
-$output = curl_exec($ch);
-$data = json_decode($output, true);
-echo $data["format"][0]["url"];
+$url = parse_url($_SERVER['REQUEST_URI']);
+if(isset($_GET['id'])){
+    $vid = $_GET['id'];
+    $ch = curl_init();
+    $str ='https://api.youtubemultidownloader.com/video?url=https://www.youtube.com/watch?v='.$vid;
+    curl_setopt($ch, CURLOPT_URL, $str);
+    curl_setopt ( $ch, CURLOPT_RETURNTRANSFER, true );
+    $output = curl_exec($ch);
+    $data = json_decode($output, true);
+    die($data["format"][0]["url"]);
+} else {
+   die("error");
+}
