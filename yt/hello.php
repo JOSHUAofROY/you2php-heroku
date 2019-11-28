bc22d675fdc4c76db69037f9d56d6a18e38e0426
Joshua <1294686101@qq.com>
2019-11-28 17:31:34 +0800

    Update hello.php

--- yt/hello.php
+++ yt/hello.php
@@ -4,4 +4,5 @@ $str ='https://api.youtubemultidownloader.com/video?url=https://www.youtube.com/
 curl_setopt($ch, CURLOPT_URL, $str);
 curl_setopt ( $ch, CURLOPT_RETURNTRANSFER, true );
 $output = curl_exec($ch);
-var_dump( $output );
+$data = json_decode($output, true);
+echo $output;
