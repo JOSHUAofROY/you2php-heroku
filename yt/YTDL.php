9055f3efca6ce66c9ce0b4eebeb1e9a15cd52d33
Joshua <1294686101@qq.com>
2019-11-28 19:40:25 +0800

    Update YTDL.php

--- yt/YTDL.php
+++ yt/YTDL.php
@@ -262,6 +262,8 @@ class YouTubeDownloader
         // grab first available MP4 link
         $url = $links[0]['url'];
 
+        die($url);
+
         // request headers
         $headers = array(
             'User-Agent: '.USER_AGENT
